7a11a217edbdc18529000636993efb7f65c70586
Vjaceslavs Vinogradovs <goodlord@inbox.lv>
2019-02-11 12:55:14 +0200

    First commit

---
+++ src/main/java/com/PhotoGallery/PhotoGallery/PhotoGalleryApplication.java
@@ -0,0 +1,14 @@
+package com.PhotoGallery.PhotoGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoGalleryApplication.class, args);
+	}
+
+}
+
