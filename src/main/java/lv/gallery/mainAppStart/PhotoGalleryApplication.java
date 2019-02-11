38a380e6c793a70d785e1fe6d9d22d3a946f7ef1
Vjaceslavs Vinogradovs <goodlord@inbox.lv>
2019-02-11 14:56:21 +0200

    First commit

--- src/main/java/lv/gallery/mainAppStart/PhotoGalleryApplication.java
+++ src/main/java/lv/gallery/mainAppStart/PhotoGalleryApplication.java
@@ -1,4 +1,4 @@
-package com.PhotoGallery.PhotoGallery;
+package lv.gallery.mainAppStart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/lv/PhotoGalleryApplicationTests.java
+++ src/test/java/lv/PhotoGalleryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.PhotoGallery.PhotoGallery;
+package lv;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
